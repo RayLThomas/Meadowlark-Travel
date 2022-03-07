5b62942548435150b1ffc9548def7d3c9b74c842
Raymond Thomas <raythomasclient@gmail.com>
2022-03-07 18:45:23 -0500

    Moved 'fortune cookie' functionalityinto module

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,50 +1,39 @@
 var express = require('express');
+var fortune = require('./lib/fortune.js');
+
 var app = express();
 
-//set up handlebars view engine
-//this creates a view engine and configures Express to use it by default
+// set up handlebars view engine
 var handlebars = require('express3-handlebars')
-  .create({ defaultLayout:'main' });
+	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-//set up static directory | pg 26
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
-//routes
 app.get('/', function(req, res) {
-    res.render('home')
+	res.render('home');
 });
-app.get('/about', function(req, res) {
-    let randomeFortune =
-        fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomeFortune});
+app.get('/about', function(req,res){
+	res.render('about', { fortune: fortune.getFortune() } );
 });
 
-//custom 404 page
-app.use(function(req, res, next) {
-    res.status(404);
-    res.render('404');
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
 });
 
-//custom 500 page
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' + 
-      app.get('port') + '; press Ctrl-C to terminate.');
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
 });
-
-//Dynamic content views example | pg 27
-var fortunes = [
-    "conquer your fears or they will conquer you.",
-    "Rivers need springs",
-    "Do not fear whad you don't know",
-    "Whenever possible, keep it simple"
-]
